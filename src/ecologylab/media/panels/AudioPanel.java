c06727e670daee4a05149f7f046f4941ba994ba0
toupsz <toupsz@ecologylab.net>
2009-03-24 17:06:37 +0000

    add menu item to set primary / secondary audio (auto mute / pan) for simplifying analysis setup

--- src/ecologylab/media/panels/AudioPanel.java
+++ src/ecologylab/media/panels/AudioPanel.java
@@ -1,35 +1,42 @@
 package ecologylab.media.panels;
-import java.awt.*;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
-import javax.swing.*;
+import javax.swing.JPanel;
 
 import ecologylab.media.AudioBufferPlayer;
 
-
 public class AudioPanel extends JPanel
 {
-	private AudioWaveformPanel waveform;
-	
+	private static final long				serialVersionUID	= 1L;
+
+	private AudioWaveformPanel			waveform;
+
+	private ChannelControllerPanel	controllerPane;
+
 	public AudioPanel(AudioBufferPlayer buffer, SoloListener s)
 	{
-		this(buffer,s,null,null);
+		this(buffer, s, null, null);
 	}
-	
+
 	public AudioPanel(AudioBufferPlayer buffer, SoloListener s, ArrayList<Integer> channels)
 	{
-		this(buffer,s,channels,null);
+		this(buffer, s, channels, null);
 	}
-	
-	public AudioPanel(AudioBufferPlayer buffer, SoloListener s, ArrayList<Integer> channels, ArrayList<String> channelNames)
+
+	public AudioPanel(AudioBufferPlayer buffer, SoloListener s, ArrayList<Integer> channels,
+			ArrayList<String> channelNames)
 	{
 		super();
-		
+
 		GridBagLayout gridbag = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
-		
+
 		this.setLayout(gridbag);
-		
+
 		c.fill = GridBagConstraints.BOTH;
 		c.gridx = 0;
 		c.gridy = 1;
@@ -37,14 +44,13 @@ public class AudioPanel extends JPanel
 		c.weighty = 1.0;
 		c.gridheight = 1;
 		c.gridwidth = 1;
-				
-		JPanel controllerPane = new ChannelControllerPanel(buffer, s, channels, channelNames);
-		controllerPane.setMinimumSize(new Dimension(120,120));
-		
+
+		controllerPane = new ChannelControllerPanel(buffer, s, channels, channelNames);
+		controllerPane.setMinimumSize(new Dimension(120, 120));
+
 		gridbag.setConstraints(controllerPane, c);
 		this.add(controllerPane);
-		
-		
+
 		waveform = new AudioWaveformPanel(buffer, channels);
 		c.gridx = 1;
 		c.gridy = 1;
@@ -54,17 +60,25 @@ public class AudioPanel extends JPanel
 		c.gridwidth = 1;
 		gridbag.setConstraints(waveform, c);
 		this.add(waveform);
-		
+
 		this.setVisible(true);
 	}
-	
+
 	public AudioPanel(AudioBufferPlayer buffer)
 	{
 		this(buffer, null);
 	}
-	
+
 	public AudioWaveformPanel getWaveform()
 	{
 		return waveform;
+	}
+
+	/**
+	 * @return the controllerPane
+	 */
+	public ChannelControllerPanel getControllerPane()
+	{
+		return controllerPane;
 	}
 }
--- src/ecologylab/media/panels/ChannelControllerPanel.java
+++ src/ecologylab/media/panels/ChannelControllerPanel.java
@@ -1,125 +1,146 @@
 package ecologylab.media.panels;
-import javax.swing.*;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.TitledBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.sound.sampled.*;
 
-import ecologylab.media.AudioBufferPlayer;
-
-import java.awt.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.sound.sampled.Control;
+import javax.sound.sampled.FloatControl;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import ecologylab.media.AudioBufferPlayer;
+
 public class ChannelControllerPanel extends JPanel implements ActionListener, ChangeListener
 {
-	private AudioBufferPlayer buffer;
-	private ArrayList<Integer> channels;
-	private boolean soloing = false;
-	
-	private JSlider volumeSlider;
-	private JSlider panSlider;
-	private JComboBox channelSelector;
-	private JCheckBox muteAllBox;
-	private JCheckBox soloBox;
-	
-	private FloatControl[] gainControls;
-	private FloatControl[] panControls;
-	private SoloListener soloer;
-	
-	private HashMap<Object, Integer> selectionMap = new HashMap<Object, Integer>();
-	
-	public static final String CHANNEL_SELECTION_COMMAND = "channel";
-	public static final String MUTE_CHANGED_COMMAND = "mute_changed";
-	public static final String SOLO_COMMAND = "solo";
-	
+	private static final long					serialVersionUID					= 1L;
+
+	private AudioBufferPlayer					buffer;
+
+	private ArrayList<Integer>				channels;
+
+	private JSlider										volumeSlider;
+
+	private JSlider										panSlider;
+
+	private JComboBox									channelSelector;
+
+	private JCheckBox									muteAllBox;
+
+	private JCheckBox									soloBox;
+
+	private FloatControl[]						gainControls;
+
+	private FloatControl[]						panControls;
+
+	private SoloListener							soloer;
+
+	private HashMap<Object, Integer>	selectionMap							= new HashMap<Object, Integer>();
+
+	public static final String				CHANNEL_SELECTION_COMMAND	= "channel";
+
+	public static final String				MUTE_CHANGED_COMMAND			= "mute_changed";
+
+	public static final String				SOLO_COMMAND							= "solo";
+
+	public static final String				MAKE_PRIMARY							= "MAKE_PRIMARY";
+
+	public static final String				MAKE_SECONDARY						= "MAKE_SECONDARY";
+
+	public static final String				RESTORE										= "RESTORE";
+
 	public ChannelControllerPanel(AudioBufferPlayer b, SoloListener s)
 	{
-		this(b,s,null,null);
+		this(b, s, null, null);
 	}
-	
+
 	public ChannelControllerPanel(AudioBufferPlayer b, SoloListener s, ArrayList<Integer> c)
 	{
-		this(b,s,c,null);
+		this(b, s, c, null);
 	}
-	
-	public ChannelControllerPanel(AudioBufferPlayer b, SoloListener s, ArrayList<Integer> ch, ArrayList<String> channelNames)
+
+	public ChannelControllerPanel(AudioBufferPlayer b, SoloListener s, ArrayList<Integer> ch,
+			ArrayList<String> channelNames)
 	{
 		this.buffer = b;
 		this.soloer = s;
-		if(ch != null)
+		if (ch != null)
 		{
 			this.channels = ch;
-		} else {
+		}
+		else
+		{
 			this.channels = new ArrayList<Integer>();
-			for(int x = 0; x < b.getChannels(); x++)
+			for (int x = 0; x < b.getChannels(); x++)
 			{
 				channels.add(x);
 			}
 		}
 		gainControls = new FloatControl[channels.size()];
-		panControls = new FloatControl[channels.size()];			
-		
+		panControls = new FloatControl[channels.size()];
+
 		Control[][] controls = b.getControls();
-		for(int x : channels)
+		for (int x : channels)
 		{
 			System.out.println("Line: " + x);
-			for(Control control : controls[x])
+			for (Control control : controls[x])
 			{
 				System.out.println(control.toString());
 				Control.Type type = control.getType();
-				if(type.equals(FloatControl.Type.MASTER_GAIN) || 
-					type.equals(FloatControl.Type.VOLUME) ||
-					type.equals(FloatControl.Type.AUX_SEND))
+				if (type.equals(FloatControl.Type.MASTER_GAIN) || type.equals(FloatControl.Type.VOLUME)
+						|| type.equals(FloatControl.Type.AUX_SEND))
 				{
-					gainControls[x] = (FloatControl)control;
+					gainControls[x] = (FloatControl) control;
 				}
-				else if(type.equals(FloatControl.Type.PAN))
+				else if (type.equals(FloatControl.Type.PAN))
 				{
 					panControls[x] = (FloatControl) control;
 				}
 			}
 		}
-		
-		
+
 		GridBagLayout gridbag = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
-		
+
 		this.setLayout(gridbag);
-		
+
 		c.fill = GridBagConstraints.BOTH;
 		c.weightx = 1.0;
 		c.gridwidth = 1;
-		
+
 		channelSelector = new JComboBox();
 		channelSelector.setEditable(false);
-		for(int x = 0; x < channels.size(); x++)
+		for (int x = 0; x < channels.size(); x++)
 		{
-			String label = (channelNames != null && x <= channelNames.size())?
-										channelNames.get(x):"Channel " + x;
+			String label = (channelNames != null && x <= channelNames.size()) ? channelNames.get(x)
+					: "Channel " + x;
 			channelSelector.addItem(label);
 			this.selectionMap.put(label, x);
 		}
-		
-		
-		
-		
+
 		muteAllBox = new JCheckBox("Mute all:");
 		muteAllBox.setHorizontalTextPosition(SwingConstants.LEFT);
 		muteAllBox.addActionListener(this);
 		muteAllBox.setActionCommand(MUTE_CHANGED_COMMAND);
 		gridbag.setConstraints(muteAllBox, c);
 		add(muteAllBox);
-		
+
 		c.gridwidth = GridBagConstraints.REMAINDER;
 		soloBox = new JCheckBox("Solo:");
 		soloBox.setHorizontalTextPosition(SwingConstants.LEFT);
 		soloBox.setActionCommand(SOLO_COMMAND);
-		
-		if(soloer != null)
+
+		if (soloer != null)
 		{
 			soloBox.addActionListener(soloer);
 			soloBox.setSelected(false);
@@ -129,44 +150,46 @@ public class ChannelControllerPanel extends JPanel implements ActionListener, Ch
 		{
 			soloBox.setEnabled(false);
 		}
-		
-		gridbag.setConstraints(soloBox,c);
+
+		gridbag.setConstraints(soloBox, c);
 		add(soloBox);
-		
+
 		c.gridwidth = GridBagConstraints.REMAINDER;
 		channelSelector.addActionListener(this);
 		channelSelector.setActionCommand(CHANNEL_SELECTION_COMMAND);
-		gridbag.setConstraints(channelSelector,c);
+		gridbag.setConstraints(channelSelector, c);
 		add(channelSelector);
-				
-		JLabel gainLabel = new JLabel("Gain:",JLabel.CENTER);
+
+		JLabel gainLabel = new JLabel("Gain:", JLabel.CENTER);
 		gridbag.setConstraints(gainLabel, c);
 		add(gainLabel);
-		
-		volumeSlider = new JSlider(0,100);
+
+		volumeSlider = new JSlider(0, 100);
 		volumeSlider.addChangeListener(this);
 		gridbag.setConstraints(volumeSlider, c);
 		add(volumeSlider);
-		
-		JLabel panLabel = new JLabel("Pan:",JLabel.CENTER);
+
+		JLabel panLabel = new JLabel("Pan:", JLabel.CENTER);
 		gridbag.setConstraints(panLabel, c);
 		add(panLabel);
-		
-		panSlider = new JSlider(-100,100);
+
+		panSlider = new JSlider(-100, 100);
 		panSlider.addChangeListener(this);
-	
+
 		gridbag.setConstraints(panSlider, c);
 		add(panSlider);
-							
-		if(gainControls != null)
+
+		if (gainControls != null)
 		{
 			volumeSlider.setEnabled(true);
-		} else {
+		}
+		else
+		{
 			volumeSlider.setEnabled(false);
 		}
-		
+
 		this.setBorder(new BevelBorder(BevelBorder.RAISED));
-		
+
 		this.setVisible(true);
 		updateControls();
 	}
@@ -175,69 +198,107 @@ public class ChannelControllerPanel extends JPanel implements ActionListener, Ch
 	{
 		return selectionMap.get(channelSelector.getSelectedItem());
 	}
-	
+
 	public void updateControls()
 	{
 		int channel = channelSelected();
-		if(panControls[channel] != null)
+		if (panControls[channel] != null)
 		{
 			panSlider.removeChangeListener(this);
-			panSlider.setValue((int)(panControls[channel].getValue()*100));
+			panSlider.setValue((int) (panControls[channel].getValue() * 100));
 			panSlider.addChangeListener(this);
-			
+
 			panSlider.setEnabled(true);
 		}
 		else
 		{
 			panSlider.setEnabled(false);
 		}
-		if(gainControls[channel] != null)
+		if (gainControls[channel] != null)
 		{
-			int volumeValue = (int) (((gainControls[channel].getValue() - gainControls[channel].getMinimum())/(gainControls[channel].getMaximum()-gainControls[channel].getMinimum())) * 100);
-			
+			int volumeValue = (int) (((gainControls[channel].getValue() - gainControls[channel]
+					.getMinimum()) / (gainControls[channel].getMaximum() - gainControls[channel].getMinimum())) * 100);
+
 			volumeSlider.removeChangeListener(this);
 			volumeSlider.setValue(volumeValue);
 			volumeSlider.addChangeListener(this);
-			
+
 			volumeSlider.setEnabled(true);
 		}
 		else
 		{
 			volumeSlider.setEnabled(false);
 		}
-		
+
 	}
-	
+
 	public void actionPerformed(ActionEvent arg0)
 	{
-		if(arg0.getActionCommand().equals(CHANNEL_SELECTION_COMMAND))
+		if (CHANNEL_SELECTION_COMMAND.equals(arg0.getActionCommand()))
 		{
 			updateControls();
-		} else if(arg0.getActionCommand().equals(MUTE_CHANGED_COMMAND))
+		}
+		else if (MUTE_CHANGED_COMMAND.equals(arg0.getActionCommand()))
 		{
-			buffer.muteAll(this.muteAllBox.isSelected() || (soloer != null && soloer.anySoloed() && !isSoloed()));
+			buffer.muteAll(this.muteAllBox.isSelected()
+					|| (soloer != null && soloer.anySoloed() && !isSoloed()));
 		}
 	}
 
 	public void stateChanged(ChangeEvent arg0)
 	{
 		int channel = channelSelected();
-		if(arg0.getSource().equals(this.volumeSlider))
+		if (arg0.getSource().equals(this.volumeSlider))
 		{
-			float value = gainControls[channel].getMinimum() + (gainControls[channel].getMaximum()-gainControls[channel].getMinimum()) * 
-																	((float)volumeSlider.getValue()/
-																	(volumeSlider.getMaximum()) - volumeSlider.getMinimum());
+			float value = gainControls[channel].getMinimum()
+					+ (gainControls[channel].getMaximum() - gainControls[channel].getMinimum())
+					* ((float) volumeSlider.getValue() / (volumeSlider.getMaximum()) - volumeSlider
+							.getMinimum());
 			this.gainControls[channel].setValue(value);
 		}
-		if(arg0.getSource().equals(this.panSlider))
+		if (arg0.getSource().equals(this.panSlider))
 		{
-			this.panControls[channel].setValue(panSlider.getValue()/100.0f);
+			this.panControls[channel].setValue(panSlider.getValue() / 100.0f);
 		}
 	}
-	
+
 	public boolean isSoloed()
 	{
 		return this.soloBox.isSelected();
 	}
+
+	public void makePrimary()
+	{
+		buffer.muteChannel(0, false);
+		buffer.muteChannel(1, true);
+		buffer.muteChannel(2, true);
+
+		this.panControls[0].setValue(1);
+
+		updateControls();
+	}
+
+	public void makeSecondary()
+	{
+		buffer.muteChannel(0, true);
+		buffer.muteChannel(1, false);
+		buffer.muteChannel(2, true);
+
+		this.panControls[1].setValue(-1);
+
+		updateControls();
+	}
 	
+	public void restore()
+	{
+		buffer.muteChannel(0, false);
+		buffer.muteChannel(1, false);
+		buffer.muteChannel(2, false);
+
+		this.panControls[0].setValue(0);
+		this.panControls[1].setValue(0);
+		this.panControls[2].setValue(0);
+
+		updateControls();
+	}
 }
